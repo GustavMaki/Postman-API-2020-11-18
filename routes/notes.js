892cb962a7e1b904935affa6bbac765f7fb48194
GustavMaki <gustav.siljamaki@gmail.com>
2020-11-18 11:00:21 +0100

    paths for GET, POST

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,23 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
+var fs = require('fs'); //File System
+
+const dataPath = "./data/notes.json";
 
 /* GET all notes listing. */
-router.get('/notes', function(req, res, next) {
-  res.send('notes coming soon...');
+router.get('/', function(req, res, next) {
+  fs.readFile(dataPath, (err,data) => {
+    if(err) {
+        throw err;
+    }
+    res.send(JSON.parse(data));
+  }) 
 });
+
+/* POST */
+router.post('/', function(req, res, next) {
+    res.send(req.body.data);
+  });
 
 module.exports = router;
