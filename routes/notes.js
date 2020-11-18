be8ba349123d72640fb03f735b6913b8262947f0
GustavMaki <gustav.siljamaki@gmail.com>
2020-11-18 15:17:28 +0100

    fixed PUT and DELETE. Problem: Records that are deleted in the JSON file just removes the record and key. So if you delete record 2 then record 1 and 3 are beside each other and doesnt change the keys to match the new record list.

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs'); //filesystem
+var id;
 
 const dataPath = "./data/notes.json";
 
 /* GET all Notes. */
 router.get('/', function(req, res, next) {
+
   fs.readFile(dataPath, (err,data) =>{
       if(err) {
           throw err;
@@ -18,20 +20,13 @@ router.get('/', function(req, res, next) {
 //POST a new Note
 router.post('/', function(req, res, next) {
 
-    //jag vill läsa in hela "filen" och ta reda på hur många poster som finns
-    //fundera på JavaScript Object vs JSON som text, dvs vi har en Array med data
-    //ta antalet poster och öka med 1
-    //detta tal blir mitt nya id
-
-    var notesdata; //= JSON.parse(fs.readFile(dataPath));
+    var notesdata; 
 
     fs.readFile(dataPath, (err,data) =>{
         if(err) {
             throw err;
         }
-        //console.log(JSON.parse(data));
         notesdata = JSON.parse(data);
-        //console.log(notesdata);
         var newNotesId = Object.keys(notesdata).length + 1;
         notesdata[newNotesId] = JSON.parse(req.body.data);
         
@@ -41,26 +36,55 @@ router.post('/', function(req, res, next) {
               console.error(err)
               return
             }
-            //file written successfully
-          })
+            res.status(200).send("new Note added!");
+        })
     });
+});
 
-    //vi FÖRVÄNTAR oss att nu är notesdata populerat med data
-    //MEN så är inte fallet!!
-    //funktionen ovan fortsätter jobba och vi hamnar hör direkt
-    //INNAN något värde har populerats till notesdata
-    //console.log(notesdata); //varför funkar inte detta????
+router.put('/', function(req, res, next) {
 
-    //console.log(notesdata[0].title);
+    var notesdata; 
 
-   //var newNotesId = Object.keys(notesdata).length + 1;
-    //notesdata[newNotesId] = req.body.data;
+    fs.readFile(dataPath, (err,data) =>{
+        if(err) {
+            throw err;
+        }
+        notesdata = JSON.parse(data);
+        notesdata[req.body.id] = JSON.parse(req.body.data);
+        
+        console.log(notesdata);
+        fs.writeFile(dataPath, JSON.stringify(notesdata), err => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            res.status(200).send("Note updated!");
+        })
+    });
+});
 
-    //fs.writeFile(dataPath, JSON.stringify(notesdata));
+router.delete('/', function(req, res, next) {
 
-    res.status(200).send("new user added successfully");
-    
-    
-  });
+    var notesdata; 
+    var notesdelete;
+    var id= req.body.id;
+
+    fs.readFile(dataPath, (err,data) =>{
+        if(err) {
+            throw err;
+        }
+        notesdata = JSON.parse(data);
+        notesdata[req.body.id] = notesdelete;
+        
+        console.log(notesdata);
+        fs.writeFile(dataPath, JSON.stringify(notesdata), err => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            res.status(200).send("Note deleted!");
+        })
+    });
+});
 
 module.exports = router;
